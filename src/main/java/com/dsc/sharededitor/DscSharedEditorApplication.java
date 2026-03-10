b319e25061e940b613f6a1cb51fae159ebbd2144
yuna <yjeong1217@gmail.com>
2026-03-10 16:58:57 +0900

    initial commit

---
+++ src/main/java/com/dsc/sharededitor/DscSharedEditorApplication.java
@@ -0,0 +1,13 @@
+package com.dsc.sharededitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscSharedEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscSharedEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsc/sharededitor/DscSharedEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsc.sharededitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscSharedEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
